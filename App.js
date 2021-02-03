d5fd728e6e58f69aa937bbbda3e818bc3c184ea3
jillianntish <jillianntish@gmail.com>
2021-02-02 18:15:49 -0600

    add button and image picker

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import cotton_logo from './assets/cotton_logo.jpg'
-import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.jpg'
+import { Image, StyleSheet, Text, View , TouchableOpacity} from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
+  
+
   return (
     <View style={styles.container}>
-      <Image source = {cotton_logo} style ={{ width: 305, height: 500 }}></Image>
-      <Text>To share a photo from your phone with a friend, just press the button below!</Text>
+      <Image source={logo} style={styles.logo} /> 
+      <Text style={styles.intro}>How does your garden grow?</Text>
+      <TouchableOpacity onPress={openImagePickerAsync} >
+        <Text style={styles.picButt}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,4 +35,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 345,
+    height: 500,
+  //  marginBottom: 15
+  },
+  intro: {
+    color: 'green',
+    fontSize: 12,
+    marginHorizontal: 20
+  },
+  picButt: {
+    fontSize: 10, 
+    color: 'white',
+    backgroundColor: 'green'
+  }
 });
